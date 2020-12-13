59a097cbfd41bcd73af04c16ce02908de2b0246b
Anthony Gamon <anthonygamon@Anthonys-MBP-2.attlocal.net>
2020-12-13 16:01:17 -0600

    updating folders

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Quesadilla Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Grilled Cheese Burger', true);
+INSERT INTO burgers (name) VALUES ('Vegi Burger');
